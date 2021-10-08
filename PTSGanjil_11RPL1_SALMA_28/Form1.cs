e32cea16962ff3db0866c62151e94c454a07800e
SalmaMuntazShaumi <80013618+SalmaMuntazShaumi@users.noreply.github.com>
2021-10-08 23:39:34 +0700

    Add project files.

---
+++ PTSGanjil_11RPL1_SALMA_28/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTSGanjil_11RPL1_SALMA_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double hasil = txt1 + txt2;
+
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double hasil = txt1 - txt2;
+
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double hasil = txt1 * txt2;
+
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+
+            double hasil = txt1 / txt2;
+
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
